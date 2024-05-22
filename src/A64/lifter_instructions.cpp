a4e11a739b5d84e1423f3b237616fc9ad2b21f8e
niansa <tuxifan@posteo.de>
2024-05-22 16:51:14 +0200

    Added runtime debug messages for memory access

--- src/A64/lifter_instructions.cpp
+++ src/A64/lifter_instructions.cpp
@@ -153,10 +153,11 @@ Value *Lifter::InstructionLifter::GetMemOpReference(bool unscaled, unsigned op_i
     else
         displacement = static_cast<int32_t>(p.PerformShift(static_cast<uint64_t>(displacement), 32, addrop.shift.type, static_cast<uint8_t>(addrop.shift.value)));
     Value *reference = rinst.builder->CreateAdd(base, p.GetRegisterView(rinst, {p.cs_handle, addrop.mem.index}));
+    Value *displaced_reference = rinst.builder->CreateAdd(reference, ConstantInt::get(reference->getType(), static_cast<uint64_t>(displacement)));
     if (insn.detail->writeback)
-        p.StoreRegister(rinst, base_reg, rinst.builder->CreateAdd(reference, ConstantInt::get(reference->getType(), static_cast<uint64_t>(displacement))));
+        p.StoreRegister(rinst, base_reg, displaced_reference);
     if (displacement && !detail.post_index)
-        reference = rinst.builder->CreateAdd(reference, rinst.builder->getInt64(static_cast<uint64_t>(displacement)));
+        reference = displaced_reference;
     return reference;
 }
 
